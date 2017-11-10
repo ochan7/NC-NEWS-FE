f35abf5ca0c84c02fbbf2e6900256f383a5946fb
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-10 23:19:58 +0000

    fetch article by topic success passes test for payload

--- spec/articleByTopic.reducer.spec.js
+++ spec/articleByTopic.reducer.spec.js
@@ -19,4 +19,22 @@ describe('articlesByTopic reducer', () => {
       expect(newState).to.equal(initialState);
     });
   });
+  it('handles FETCH_ARTICLES_BY_TOPIC_REQUEST', () => {
+    const topic = 'football';
+    const action = fetchArticlesByTopicRequest(topic);
+    const newState = articlesByTopicReducer(undefined, action);
+    expect(newState.loading).to.be.true;
+    expect(newState.error).to.be.null;
+    expect(newState.data).to.eql([]);
+  });
+  it('handles FETCH_ARTICLES_BY_TOPIC_SUCCESS', () => {
+    const topic = 'football';
+    const prevState = articlesByTopicReducer(undefined, fetchArticlesByTopicRequest(topic));
+    const data = [1,2,3];
+    const action = fetchArticlesByTopicSuccess(data);
+    const newState = articlesByTopicReducer(prevState, action);
+    expect(newState.loading).to.be.false;
+    expect(newState.error).to.be.null;
+    expect(newState.data).to.eql(data);
+  });
 });
