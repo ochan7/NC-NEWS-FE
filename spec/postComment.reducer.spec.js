465b9a2485c789f8f0c6f2cc899dfbf3e1379d84
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-11 18:54:37 +0000

    post comment reducer passes test for request

--- spec/postComment.reducer.spec.js
+++ spec/postComment.reducer.spec.js
@@ -8,6 +8,7 @@ import {
 
 describe.only('postComment reducer', () => {
   const article_id = '5a033990e03644b9fab5289c';
+  const comment = 'this is a comment';
   describe('default behaviour', () => {
     it('returns the passed previous state if an unrecognised action is passed', () => {
       const action = {type: 'whatever'};
@@ -20,4 +21,12 @@ describe.only('postComment reducer', () => {
       expect(newState).to.equal(initialState);
     });
   });
+  it('handles the POST_COMMENT_REQUEST', () => {
+    const action = postCommentRequest(article_id, comment);
+    const newState = postCommentReducer(undefined, action);
+    expect(newState.loading).to.be.true;
+    expect(newState.error).to.be.null;
+    expect(newState.data).to.eql([]);
+  });
+
 });
