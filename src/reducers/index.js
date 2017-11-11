8618c218d027a2c987aa98aafad552f4f97e25f1
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-11 22:20:09 +0000

    add put comment types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,9 +7,16 @@ import getComments from './getComments';
 import getTopics from './getTopics';
 import postComment from './postComment';
 import getUser from './getUser';
-
+import putArticle from './putArticle';
 const reducer = combineReducers({
-  test, getArticles, getArticlesByTopic, getComments, getTopics, postComment, getUser
+  test, 
+  getArticles, 
+  getArticlesByTopic, 
+  getComments, 
+  getTopics, 
+  postComment, 
+  getUser,
+  putArticle
 });
 
 export default reducer;
