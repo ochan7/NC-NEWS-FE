1c873c949b9f0184da6daa66f50fd962c65b5e63
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-12 20:38:56 +0000

    fix pagination page numbers

--- src/components/HomePageUI.js
+++ src/components/HomePageUI.js
@@ -4,7 +4,9 @@ const HomePageUI = ({articles}) => (
   <div>
     {articles.map(article => (
       <div key={article._id}>
-        <p>{article.title}</p>
+        <h3>{article.title}</h3>
+        <p>{article.body.slice(0, 100)} ...</p>
+        <p>Popularity {article.votes}</p>
       </div>
     ))}
   </div>
