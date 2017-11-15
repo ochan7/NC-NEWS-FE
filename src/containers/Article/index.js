a2be1b7fd57627fc13ba6d786f7644931a2f5166
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-15 15:47:02 +0000

    refactor vote props

--- src/containers/Article/index.js
+++ src/containers/Article/index.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 import getArticle from '../../actions/getArticle';
 import Votes from '../Votes/index';
 import Loading from '../../components/Loading';
+import putArticle from '../../actions/putArticle';
 class Article extends Component {
   constructor (props) {
     super(props);
@@ -14,7 +15,7 @@ class Article extends Component {
     this.props.getArticle(article_id);
   }
   render () {
-    const {article, loading} = this.props;
+    const {article, loading, voteError, voteLoading, putArticle, votes} = this.props;
     return (
 
       <div>
@@ -22,7 +23,15 @@ class Article extends Component {
           <Loading/> :
           <div>
             <ArticleUI article = {article}/>
-            <Votes article = {article}/>
+            <Votes 
+              votes = {
+                votes === undefined ? article.votes : votes
+              }
+              id = {article._id}
+              putData={putArticle}
+              loading = {voteLoading}
+              error = {voteError}
+            />
           </div>
         }
       </div>
@@ -34,16 +43,26 @@ Article.propTypes = {
   article: PT.object.isRequired,
   loading: PT.bool.isRequired,
   error: PT.any.isRequired,
-  getArticle: PT.func.isRequired
+  getArticle: PT.func.isRequired,
+  putArticle: PT.func.isRequired,
+  votes:PT.number.isRequired,
+  voteLoading: PT.bool.isRequired,
+  voteError: PT.any.isRequired
 };
 const mapStateToProps = state => ({
   article: state.getArticle.data,
   loading: state.getArticle.loading,
-  error: state.getArticle.error
+  error: state.getArticle.error,
+  votes: state.putArticle.data.votes,
+  voteLoading: state.putArticle.loading,
+  voteError: state.putArticle.error   
 });
 const mapDispatchToProps = dispatch => ({
   getArticle: (article_id) => {
     dispatch(getArticle(article_id));
+  },
+  putArticle: (id, vote) => {
+    dispatch(putArticle(id, vote));
   }
 });
 
--- src/containers/Votes/index.js
+++ src/containers/Votes/index.js
@@ -1,23 +1,22 @@
 import VotesUI from '../../components/VotesUI';
 import React, {Component} from 'react';
 import PT from 'prop-types';
-import {connect} from 'react-redux';
-import putArticle from '../../actions/putArticle';
 
 class Votes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      votes: this.props.article.votes
+      votes: this.props.votes
     };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(vote){
-    const {putArticle, article : {_id: article_id}} = this.props;
-    return () => putArticle(article_id, vote);
+    const {putData, id} = this.props;
+    return () => putData(id, vote);
   }
   componentWillReceiveProps(nextProps){
     if(typeof nextProps.votes === 'number'){
+      console.log(nextProps.votes);
       this.setState({
         votes: nextProps.votes
       });
@@ -36,20 +35,11 @@ class Votes extends Component {
   }
 }
 Votes.propTypes = {
-  article: PT.object.isRequired,
+  id: PT.string.isRequired,
   loading: PT.bool.isRequired,
   error: PT.any.isRequired,
-  putArticle: PT.func.isRequired,
+  putData: PT.func.isRequired,
   votes: PT.number.isRequired
 };
-const mapStateToProps = state => ({
-  votes: state.putArticle.data.votes,
-  loading: state.putArticle.loading,
-  error: state.putArticle.error   
-});
-const mapDispatchToProps = dispatch => ({
-  putArticle: (article_id, vote) => {
-    dispatch(putArticle(article_id, vote));
-  }
-});
-export default connect(mapStateToProps, mapDispatchToProps)(Votes);
+
+export default Votes;
