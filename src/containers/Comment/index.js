efa8f7c6bd52ebe04bb921877c1d454f6930af32
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-15 21:53:34 +0000

    add votes for comments

--- src/containers/Comment/index.js
+++ src/containers/Comment/index.js
@@ -1,45 +1,51 @@
 import React, {Component} from 'react';
 import PT from 'prop-types';
 import CommentUI from '../../components/CommentUI';
-// import {connect} from 'react-redux';
-// import putComment from '../../actions/putComment';
-// import Votes from '../Votes/index';
+import {connect} from 'react-redux';
+import putComment from '../../actions/putComment';
+import VotesUI from '../../components/VotesUI';
 class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: this.props.comment.votes
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(value){
+    const {_id: id} = this.props.comment;
+    let increment;
+    value === 'up' ? increment = 1 : increment =-1;
+    return () => {
+      const {votes} = this.state;
+      const {putComment} = this.props;
+      putComment(id, value);
+      this.setState({
+        votes: votes + increment
+      });
+    };
+  }
   render() {
     const {comment} = this.props;
-    // const {votes, loading, putComment, error} = this.props;
+
     return (
       <div>
         <CommentUI comment={comment}/>
-        {/* <Votes
-          votes = {
-            votes === undefined ? comment.votes : votes
-          }
-          id = {comment._id}
-          putData={putComment}
-          loading = {loading}
-          error = {error}
-        /> */}
+        <VotesUI votes = {this.state.votes} loading = {false} handleClick = {this.handleClick}/>
       </div>
     );
   }
-} 
-      
-// const mapStateToProps = state => ({
-//   votes: state.putComment.data.votes,
-//   loading: state.putComment.loading,
-//   error: state.putComment.error,
-// });
+}
 
-// const mapDispatchToProps = dispatch => ({
-//   putComment: (id, vote) => {
-//     dispatch(putComment(id, vote));
-//   }
-// });
+const mapDispatchToProps = dispatch => ({
+  putComment: (id, vote) => {
+    dispatch(putComment(id, vote));
+  }
+});
 
 Comment.propTypes = {
   comment: PT.object.isRequired,
-
+  putComment: PT.func.isRequired
 };
-// export default connect(mapStateToProps, mapDispatchToProps)(Comment);
-export default Comment;
+export default connect(null, mapDispatchToProps)(Comment);
+// export default Comment;
