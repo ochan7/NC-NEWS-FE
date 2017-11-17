46b86153a74399b1eb0e8ad3e4db160b89f26b0c
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-17 01:27:07 +0000

    add reducer for get user data

---
+++ src/reducers/getUserData.js
@@ -0,0 +1,32 @@
+import * as types from '../actions/types';
+
+export const initialState = {
+  loading: false,
+  error: null,
+  data: []
+};
+
+export default (prevState = initialState, action) => {
+  switch (action.type) {
+  case types.GET_USER_DATA_REQUEST:
+    return Object.assign({}, prevState, {
+      loading: !prevState.loading,
+      error: null,
+      data: []
+    });
+  case types.GET_USER_DATA_SUCCESS:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: null,
+      data: action.payload
+    });
+  case types.GET_USER_DATA_FAILURE:
+    return Object.assign({}, prevState, {
+      loading: false,
+      error: action.payload,
+      data: []
+    });
+  default:
+    return prevState;
+  }
+};
