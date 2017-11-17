e1589173e6a90504551d6e079f05cb0599dec391
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-17 01:25:02 +0000

    add action for get user data

---
+++ src/actions/getUserData.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import * as types from './types';
+import {API_URL} from '../../config';
+
+export const getUserDataRequest = (username) => ({
+  type: types.GET_USER_DATA_REQUEST,
+  payload: username
+});
+
+export const getUserDataSuccess = (data) => ({
+  type: types.GET_USER_DATA_SUCCESS,
+  payload: data
+});
+
+export const getUserDataFailure = (error) => ({
+  type: types.GET_USER_DATA_FAILURE,
+  payload: error
+});
+
+export default (username) => {
+  return (dispatch) => {
+    dispatch(getUserDataRequest(username));
+    return axios.get(`${API_URL}/users/${username}/repos`)
+      .then(({data}) => {
+        dispatch(getUserDataSuccess(data.users));
+      })
+      .catch(error => {
+        dispatch(getUserDataFailure(error.message));
+      });
+  };
+};
